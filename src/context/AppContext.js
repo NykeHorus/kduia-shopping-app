1f38311d1e8a5db90e2630c1a9474c3df6a62c7f
Muhammad Hariss <112217564+NykeHorus@users.noreply.github.com>
2023-02-23 07:03:05 -0500

    fixed package Json and fixed typo in AppContext

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,60 +1,59 @@
 import React, { createContext, useReducer } from 'react';
 
-// 5. The reducer -this is used to update the state, based on the action
-export const AppReducer = (state,action) => {
-    let new_expenses =[];
+// 5. The reducer - this is used to update the state, based on the action
+export const AppReducer = (state, action) => {
+    let new_expenses = [];
     switch (action.type) {
-        case 'Add_Quantity':
+        case 'ADD_QUANTITY':
             let updatedqty = false;
-            state.expenses.map((expense) =>{
-                if(expense.name ===action.playload.name) {
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
                     expense.quantity = expense.quantity + action.payload.quantity;
-                    updattedqty = true;
+                    updatedqty = true;
                 }
                 new_expenses.push(expense);
+                return true;
             })
-            state.expenses =new_expenses;
-            action.type ="Done";
+            state.expenses = new_expenses;
+            action.type = "DONE";
             return {
                 ...state,
             };
-
             case 'RED_QUANTITY':
-                state.expenses.map((expense)=> {
+                state.expenses.map((expense)=>{
                     if(expense.name === action.payload.name) {
                         expense.quantity = expense.quantity - action.payload.quantity;
                     }
-                    expense.quantity = expense.quantity <0 ? 0: expense.quantity;
+                    expense.quantity = expense.quantity < 0 ? 0: expense.quantity;
                     new_expenses.push(expense);
                     return true;
                 })
                 state.expenses = new_expenses;
-                action.type = "Done";
+                action.type = "DONE";
                 return {
                     ...state,
                 };
-                case 'DELETE_ITEM':
-                state.expenses.map((expense)=>{
-                    if(expense.name === action.payload.name) {
-                        expense.quantity = 0;
-                    }
-                    new_expenses.push(expense);
-                    return true;
-                })
-                state.expenses= new_expenses;
-                action.type="Done";
-                return{
-                    ...state,
-                };
-                case 'CHG_LOCATION':
-                    action.type="Done";
-                    state.location = action.payload;
-                    return {
-                        ...state
-                    }
-
-                    default:
-                        return state;
+        case 'DELETE_ITEM':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = 0;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
+    case 'CHG_LOCATION':
+            action.type = "DONE";
+            state.Location = action.payload;
+            return {
+                ...state
+            }
+        default:
+            return state;
     }
 };
 
